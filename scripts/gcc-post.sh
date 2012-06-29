bf3de1c0f764393e2fa120c12106fe0ec13d34ae
Alexpux <alexpux@gmail.com>
2012-06-29 20:41:58 +0400

    Add support to build MULTILIB and NOMULTILIB toolchains

--- scripts/gcc-post.sh
+++ scripts/gcc-post.sh
@@ -39,78 +39,46 @@
 	# remove <prefix>/mingw directory
 	rm -rf $PREFIX/mingw >/dev/null 2>&1
 	
-	[[ $USE_DWARF_EXCEPTIONS == no ]] && {
-		[[ $ARCHITECTURE == x32 ]] && {
-			# libgcc_s.a
-			cp -f $PREFIX/lib/gcc/$TARGET/lib/libgcc_s.a $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $PREFIX/lib/gcc/$TARGET/lib64/libgcc_s.a $PREFIX/$TARGET/lib64/ || exit 1
+	[[ $ARCHITECTURE == x32 ]] && {
+		# libgcc_s.a
+		cp -f $PREFIX/lib/gcc/$TARGET/lib/libgcc_s.a $PREFIX/$TARGET/lib/ || exit 1
 
+		[[ $USE_DWARF_EXCEPTIONS == no ]] && {
 			# 32bit dlls
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgcc/shlib/libgcc_s_sjlj-1.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgcc/shlib/libgcc_s_sjlj-1.dll $PREFIX/$TARGET/lib/ || exit 1
-			[[ $ENABLE_LANGUAGES =~ fortran ]] && {
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgfortran/.libs/libgfortran-3.dll $PREFIX/bin/ || exit 1
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgfortran/.libs/libgfortran-3.dll $PREFIX/$TARGET/lib/ || exit 1
-			}
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgomp/.libs/libgomp-1.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgomp/.libs/libgomp-1.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libquadmath/.libs/libquadmath-0.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libquadmath/.libs/libquadmath-0.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libssp/.libs/libssp-0.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libssp/.libs/libssp-0.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/$TARGET/lib/ || exit 1
-
-			# 64bit dlls
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libgcc/64/shlib/libgcc_s_sjlj-1.dll $PREFIX/$TARGET/lib64/ || exit 1
-			[[ $ENABLE_LANGUAGES =~ fortran ]] && {
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libgfortran/.libs/libgfortran-3.dll $PREFIX/$TARGET/lib64/ || exit 1
-			}
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libgomp/.libs/libgomp-1.dll $PREFIX/$TARGET/lib64/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libquadmath/.libs/libquadmath-0.dll $PREFIX/$TARGET/lib64/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libssp/.libs/libssp-0.dll $PREFIX/$TARGET/lib64/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/64/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/$TARGET/lib64/ || exit 1
+			cp -rf `find $BUILD_DIR/$GCC_NAME/$TARGET -path $BUILD_DIR/$GCC_NAME/$TARGET/64 -prune -o -type f -name "*.dll"` $PREFIX/bin/ || exit 1
+			cp -rf `find $BUILD_DIR/$GCC_NAME/$TARGET -path $BUILD_DIR/$GCC_NAME/$TARGET/64 -prune -o -type f -name "*.dll"` $PREFIX/$TARGET/bin/ || exit 1
 			
 			strip $PREFIX/bin/*.dll || exit 1
 			strip $PREFIX/$TARGET/lib/*.dll || exit 1
-			strip $PREFIX/$TARGET/lib64/*.dll || exit 1
-		} || {
-			# libgcc_s.a
-			cp -f $PREFIX/lib/gcc/$TARGET/lib/libgcc_s.a $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $PREFIX/lib/gcc/$TARGET/lib32/libgcc_s.a $PREFIX/$TARGET/lib32/ || exit 1
 
-			# 64bit dlls
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgcc/shlib/libgcc_s_sjlj-1.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgcc/shlib/libgcc_s_sjlj-1.dll $PREFIX/$TARGET/lib/ || exit 1
-			[[ $ENABLE_LANGUAGES =~ fortran ]] && {
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgfortran/.libs/libgfortran-3.dll $PREFIX/bin/ || exit 1
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgfortran/.libs/libgfortran-3.dll $PREFIX/$TARGET/lib/ || exit 1
+			# 64 bit files
+			[[ $USE_MULTILIB_MODE == yes ]] && {
+				# libgcc_s.a
+				cp -f $PREFIX/lib/gcc/$TARGET/lib64/libgcc_s.a $PREFIX/$TARGET/lib64/ || exit 1
+				cp -rf `find $BUILD_DIR/$GCC_NAME/$TARGET/64 -type f \( -iname "*.dll ! -iname "*winpthread*" \)` $PREFIX/$TARGET/lib64/ || exit 1
+				
+				strip $PREFIX/$TARGET/lib64/*.dll || exit 1
 			}
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgomp/.libs/libgomp-1.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libgomp/.libs/libgomp-1.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libquadmath/.libs/libquadmath-0.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libquadmath/.libs/libquadmath-0.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libssp/.libs/libssp-0.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libssp/.libs/libssp-0.dll $PREFIX/$TARGET/lib/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/bin/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/$TARGET/lib/ || exit 1
-
-			# 32bit dlls
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libgcc/32/shlib/libgcc_s_sjlj-1.dll $PREFIX/$TARGET/lib32/ || exit 1
-			[[ $ENABLE_LANGUAGES =~ fortran ]] && {
-				cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libgfortran/.libs/libgfortran-3.dll $PREFIX/$TARGET/lib32/ || exit 1
-			}
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libgomp/.libs/libgomp-1.dll $PREFIX/$TARGET/lib32/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libquadmath/.libs/libquadmath-0.dll $PREFIX/$TARGET/lib32/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libssp/.libs/libssp-0.dll $PREFIX/$TARGET/lib32/ || exit 1
-			cp -f $BUILDS_DIR/$GCC_NAME/$TARGET/32/libstdc++-v3/src/.libs/libstdc++-6.dll $PREFIX/$TARGET/lib32/ || exit 1
-
-			strip $PREFIX/bin/*.dll || exit 1
-			strip $PREFIX/$TARGET/lib/*.dll || exit 1
-			strip $PREFIX/$TARGET/lib32/*.dll || exit 1
 		}
 	} || {
+		# libgcc_s.a
 		cp -f $PREFIX/lib/gcc/$TARGET/lib/libgcc_s.a $PREFIX/$TARGET/lib/ || exit 1
+			
+		# 64bit dlls
+		cp -rf `find $BUILDS_DIR/$GCC_NAME/$TARGET -path $BUILD_DIR/$GCC_NAME/$TARGET/32 -prune -o -type f -name "*.dll"` $PREFIX/bin/ || exit 1
+		cp -rf `find $BUILDS_DIR/$GCC_NAME/$TARGET -path $BUILD_DIR/$GCC_NAME/$TARGET/32 -prune -o -type f -name "*.dll"` $PREFIX/$TARGET/lib/ || exit 1
+			
+		strip $PREFIX/bin/*.dll || exit 1
+		strip $PREFIX/$TARGET/lib/*.dll || exit 1
+			
+		# 32 bit files
+		[[ $USE_MULTILIB_MODE ==yes ]] && {
+			# libgcc_s.a
+			cp -f $PREFIX/lib/gcc/$TERGET/lib32/libgcc_s.a $PREFIX/$TARGET/lib32/ || exit 1
+			cp -rf `find $BUILD_DIR/$GCC_NAME/$TARGET/32 -type f \( -iname "*.dll" ! -iname "*winpthread*" \)` $PREFIX/$TARGET/lib32/ || exit 1
+			
+			strip $PREFIX/$TARGET/lib32/*.dll || exit 1
+		}
 	}
 	
 	touch $BUILDS_DIR/gcc-post.marker
