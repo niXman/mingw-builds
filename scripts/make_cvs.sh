e63c2015c83e836174473836d5391d87fafd3e71
Alexpux <alexpux@gmail.com>
2012-06-29 21:09:48 +0400

    Add support to build MULTILIB and NOMULTILIB toolchains. Add script and patch for build GNU Make from cvs.

---
+++ scripts/make_cvs.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#
+# The BSD 3-Clause License. http://www.opensource.org/licenses/BSD-3-Clause
+#
+# This file is part of 'mingw-builds' project.
+# Copyright (c) 2011,2012, by niXman (i dotty nixman doggy gmail dotty com)
+# All rights reserved.
+#
+# Project: mingw-builds ( http://sourceforge.net/projects/mingwbuilds/ )
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# - Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the distribution.
+# - Neither the name of the 'mingw-builds' nor the names of its contributors may 
+#     be used to endorse or promote products derived from this software 
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# **************************************************************************
+
+NAME=make
+SRC_DIR_NAME=make
+URL=":pserver:anonymous:@cvs.sv.gnu.org:/sources/make"
+TYPE=cvs
+REV=01/01/2012
+MODULES=()
+#
+
+#rm -rf /home/$USERNAME/.cvspass
+#echo "/1 :pserver:anonymous@cvs.sv.gnu.org:2401/sources/make A" > /home/$USERNAME/.cvspass
+
+#cd $SRCS_DIR
+
+#cvs co -D'01/01/2012' make
+
+PATCHES=(make-postcvs.patch)
+
+EXECUTE_AFTER_PATCH=("cp -rf $PATCHES_DIR/make/doc/* $SRCS_DIR/make/doc/" "autoreconf -i")
+#
+
+CONFIGURE_FLAGS=(
+	--host=$HOST
+	--build=$TARGET
+	--prefix=$PREFIX
+	--enable-case-insensitive-file-system
+	--program-prefix=mingw32-
+	--enable-job-server
+	CFLAGS="\"$COMMON_CFLAGS\""
+	LDFLAGS="\"$COMMON_LDFLAGS -L$LIBS_DIR/lib\""
+)
+
+#
+
+MAKE_FLAGS=(
+	-j$JOBS
+	all
+)
+
+#
+
+INSTALL_FLAGS=(
+	-j$JOBS
+	install
+)
+
+#
+
+#export CVSROOT=:pserver:anonymous@cvs.sv.gnu.org:/sources/make
