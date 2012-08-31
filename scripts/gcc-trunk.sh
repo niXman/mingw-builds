f58e7bc0f0ffd7ae0a7646a870eef7dbe07aceb0
niXman <i.nixman@gmail.com>
2012-08-31 21:48:43 +0400

    remove gdb_cvs

--- scripts/gcc-trunk.sh
+++ scripts/gcc-trunk.sh
@@ -70,10 +70,11 @@ CONFIGURE_FLAGS=(
 	--enable-checking=release
 	--enable-fully-dynamic-string
 	--enable-version-specific-runtime-libs
-	$( [[ $USE_DWARF_EXCEPTIONS == yes ]] \
-		&& echo "--disable-sjlj-exceptions --with-dwarf2" \
-		|| echo "--enable-sjlj-exceptions" \
-	)
+#	$( [[ $USE_DWARF_EXCEPTIONS == yes ]] \
+#		&& echo "--disable-sjlj-exceptions --with-dwarf2" \
+#		|| echo "--enable-sjlj-exceptions" \
+#	)
+#	--with-dwarf2
 	#
 	--disable-ppl-version-check
 	--disable-cloog-version-check
