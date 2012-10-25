ecac625f0bb7b762bdabd458cdb5870325be5746
Alexey Pavlov <alexey.pawlow@gmail.com>
2012-10-25 06:56:56 +0400

    Add suffix to execute logs

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -185,10 +185,11 @@ function func_execute {
 	# $1 - execute dir
 	# $2 - src dir name
 	# $3 - message
-	# $4 - commands list
+	# $4 - log suffix
+	# $5 - commands list
 
 	local _result=0
-	local -a _commands=( "${!4}" )
+	local -a _commands=( "${!5}" )
 	declare -i _index=${#_commands[@]}-1
 	local _cmd_marker_name=$1/$2/exec-$_index.marker
 
@@ -204,7 +205,7 @@ function func_execute {
 
    for it in "${_commands[@]}"; do
 		_cmd_marker_name=$1/$2/exec-$_index.marker
-		local _cmd_log_name=$LOGS_DIR/$2/exec-$_index.log
+		local _cmd_log_name=$LOGS_DIR/$2/exec-$4-$_index.log
 
       [[ ! -f $_cmd_marker_name ]] && {
          ( cd $1/$2 && eval ${it} > $_cmd_log_name 2>&1 )
