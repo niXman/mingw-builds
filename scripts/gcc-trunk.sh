64bb015f8695e7d9604d127d8ef1b4add7162559
Alexey Pavlov <alexey.pawlow@gmail.com>
2012-12-11 08:59:25 +0400

    Remove patches because they are applied to trunk

--- scripts/gcc-trunk.sh
+++ scripts/gcc-trunk.sh
@@ -46,9 +46,7 @@ PRIORITY=main
 PATCHES=(
 	gcc/gcc-4.7-stdthreads.patch
 	gcc/gcc-4.8-iconv.patch
-	gcc/gcc-4.8-build-with-c++.patch
 	gcc/gcc-4.8-libstdc++export.patch
-	gcc/gcc-4.8-stmt.c.patch
 )
 
 #
