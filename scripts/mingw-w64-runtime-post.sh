c1c25d1a6af94e87c0d331e33ebced4aa8c5e8d5
Алексей Павлов <alexey.pawlow@gmail.com>
2013-01-09 09:04:27 +0400

    Use TARGET when create directories instead HOST

--- scripts/mingw-w64-runtime-post.sh
+++ scripts/mingw-w64-runtime-post.sh
@@ -49,7 +49,7 @@
 		RUNTIMEPREFIX=$RUNTIME_DIR/$ARCHITECTURE-mingw-w64-nomulti
 	}
 	
-	cp -rf $RUNTIMEPREFIX/* $PREFIX/$HOST || exit 1
+	cp -rf $RUNTIMEPREFIX/* $PREFIX/$TARGET || exit 1
 	cp -rf $RUNTIMEPREFIX/* $PREFIX/mingw || exit 1
 	
 	[[ $USE_MULTILIB == yes ]] && {
