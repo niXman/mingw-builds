0c4ca412894aed9e364d74d5ecb5bf3e3cb6dde7
Alexpux <alexey.pawlow@gmail.com>
2013-09-17 14:07:19 +0400

    Fix scripts for new architectures

--- scripts/libiconv-x64.sh
+++ scripts/libiconv-x64.sh
@@ -36,7 +36,7 @@
 # **************************************************************************
 
 VERSION=1.14
-NAME=x64-libiconv-$LINK_TYPE_SUFFIX
+NAME=x86_64-libiconv-$LINK_TYPE_SUFFIX
 SRC_DIR_NAME=libiconv-${VERSION}
 TYPE=.tar.gz
 URL=(
