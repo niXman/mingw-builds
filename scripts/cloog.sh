f4e6f92eefc190261130978e6f5811547959135b
Alexpux <alexey.pawlow@gmail.com>
2013-10-15 15:10:17 +0400

    Update cloog to 0.18.1

--- scripts/cloog.sh
+++ scripts/cloog.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=0.18.0
+PKG_VERSION=0.18.1
 PKG_NAME=$BUILD_ARCHITECTURE-cloog-${PKG_VERSION}-$LINK_TYPE_SUFFIX
 PKG_DIR_NAME=cloog-${PKG_VERSION}
 PKG_TYPE=.tar.gz
