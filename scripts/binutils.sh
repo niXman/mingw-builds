ff4a5dc091026b54f159382ca0cf3e3131aa21de
niXman <i.nixman@autistici.org>
2015-01-14 12:06:12 +0200

    binutils updated to 2.25

--- scripts/binutils.sh
+++ scripts/binutils.sh
@@ -9,33 +9,33 @@
 #
 # Project: MinGW-W64 ( http://sourceforge.net/projects/mingw-w64/ )
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# - Redistributions of source code must retain the above copyright 
+# - Redistributions of source code must retain the above copyright
 #     notice, this list of conditions and the following disclaimer.
-# - Redistributions in binary form must reproduce the above copyright 
-#     notice, this list of conditions and the following disclaimer in 
+# - Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
 #     the documentation and/or other materials provided with the distribution.
-# - Neither the name of the 'MinGW-W64' nor the names of its contributors may 
-#     be used to endorse or promote products derived from this software 
+# - Neither the name of the 'MinGW-W64' nor the names of its contributors may
+#     be used to endorse or promote products derived from this software
 #     without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 # A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
 # **************************************************************************
 
-PKG_VERSION=2.24
+PKG_VERSION=2.25
 PKG_NAME=binutils-${PKG_VERSION}
 [[ $USE_MULTILIB == yes ]] && {
 	PKG_NAME=$BUILD_ARCHITECTURE-$PKG_NAME-multi
@@ -53,9 +53,11 @@ PKG_PRIORITY=prereq
 #
 
 PKG_PATCHES=(
-	binutils/100-dont-escape-arguments-that-dont-need-it-in-pex-win32.c.patch
-	binutils/120-Bug-16858-weak-external-reference-has-wrong-value.patch
-	binutils/130-make-relocbase-unsigned.patch
+	binutils/0001-enable-gold-on.mingw32.patch
+	binutils/0002-check-for-unusual-file-harder.patch
+	binutils/0008-fix-libiberty-makefile.mingw.patch
+	binutils/0009-fix-libiberty-configure.mingw.patch
+	binutils/0110-binutils-mingw-gnu-print.patch
 )
 
 #
@@ -86,11 +88,15 @@ PKG_CONFIGURE_FLAGS=(
 	)
 	#
 	--enable-lto
+	--enable-plugins
+	--enable-gold
+	--enable-install-libiberty
 	#
 	--with-libiconv-prefix=$PREREQ_DIR/libiconv-$BUILD_ARCHITECTURE
 	#
 	--disable-rpath
 	--disable-nls
+	--disable-shared
 	#
 	$LINK_TYPE_GCC
 	#
