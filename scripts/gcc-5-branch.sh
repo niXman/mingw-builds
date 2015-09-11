cb65255fa289d47209b4123e820e8ec426cae2da
niXman <i.nixman@autistici.org>
2015-09-11 14:03:32 +0300

    fixes for gcc-5 & gcc-6 build scripts

--- scripts/gcc-5-branch.sh
+++ scripts/gcc-5-branch.sh
@@ -56,7 +56,11 @@ PKG_PATCHES=(
 	gcc/gcc-4.8.2-prettify-linking-no-undefined.mingw.patch
 	gcc/gcc-4.9.1-enable-shared-gnat-implib.mingw.patch
 	gcc/gcc-5.1.0-make-xmmintrin-header-cplusplus-compatible.patch
+	gcc/gcc-5.1.0-detect-sjlj-cleanup.patch
 	gcc/ktietz-libgomp.patch
+	gcc/gcc-5.2-fix-mingw-pch.patch
+	gcc/gcc-5-dwarf-regression.patch
+	gcc/gcc-5.1.0-fix-libatomic-building-for-threads=win32.patch
 )
 
 #
--- scripts/gcc-trunk.sh
+++ scripts/gcc-trunk.sh
@@ -58,6 +58,7 @@ PKG_PATCHES=(
 	gcc/gcc-6-ktietz-libgomp.patch
 	gcc/gcc-5.2-fix-mingw-pch.patch
 	gcc/gcc-5-dwarf-regression.patch
+	gcc/gcc-5.1.0-fix-libatomic-building-for-threads=win32.patch
 )
 
 #
