36412ffb5970dba551a282ea068cadcf04db790a
Andy Neebel <andy@andne.net>
2016-03-25 22:37:14 -0500

    Use ISL 0.14 for GCC 4

--- scripts/isl.sh
+++ scripts/isl.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=0.15
+PKG_VERSION=$( [[ $GCC_NAME == gcc-4* ]] && { echo 0.14.1; } || { echo 0.15; } )
 PKG_NAME=$BUILD_ARCHITECTURE-isl-${PKG_VERSION}-$LINK_TYPE_SUFFIX
 PKG_DIR_NAME=isl-${PKG_VERSION}
 PKG_TYPE=.tar.xz
