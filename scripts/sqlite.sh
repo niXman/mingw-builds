c4f0a889f11e56edfd17406292ef165e626b0374
Furniel <furniel@mail.ru>
2017-10-11 13:37:41 +0300

    sqlite: update to 3200100

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -35,12 +35,12 @@
 
 # **************************************************************************
 
-PKG_VERSION=3081002
+PKG_VERSION=3200100
 PKG_NAME=sqlite-${PKG_VERSION}
 PKG_DIR_NAME=sqlite-autoconf-${PKG_VERSION}
 PKG_TYPE=.tar.gz
 PKG_URLS=(
-	"https://www.sqlite.org/2015/sqlite-autoconf-${PKG_VERSION}.tar.gz"
+	"https://www.sqlite.org/2017/sqlite-autoconf-${PKG_VERSION}.tar.gz"
 )
 PKG_PRIORITY=extra
 
