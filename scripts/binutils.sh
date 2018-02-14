4ec992d178389bf63fd0f6c66363fbd28a51dff5
furniel <furniel@mail.ru>
2018-02-14 15:27:18 +0200

    binutils: update to 2.30

--- scripts/binutils.sh
+++ scripts/binutils.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.29.1
+PKG_VERSION=2.30
 PKG_NAME=binutils-${PKG_VERSION}
 [[ $USE_MULTILIB == yes ]] && {
 	PKG_NAME=$BUILD_ARCHITECTURE-$PKG_NAME-multi
