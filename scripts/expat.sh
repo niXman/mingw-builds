f780f80b3eefff716025e7aec9fa87593d69ce8d
furniel <furniel@mail.ru>
2018-02-14 15:45:21 +0200

    expat: update to 2.2.5

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.2.4
+PKG_VERSION=2.2.5
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.bz2
