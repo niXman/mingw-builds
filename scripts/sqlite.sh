27d5ea95bfbc744eb4d9277c36cc38e1f1b9cde8
Furniel <furniel@mail.ru>
2018-06-18 20:17:07 +0300

    sqlite: update to 3240000

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=3230100
+PKG_VERSION=3240000
 PKG_NAME=sqlite-${PKG_VERSION}
 PKG_DIR_NAME=sqlite-autoconf-${PKG_VERSION}
 PKG_TYPE=.tar.gz
