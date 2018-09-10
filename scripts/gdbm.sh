8b395b6921e9e786eb9aaf75ff31cb05a11a9d9c
Furniel <furniel@mail.ru>
2018-09-10 15:22:46 +0300

    gdbm: update to 1.18

--- scripts/gdbm.sh
+++ scripts/gdbm.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=1.17
+PKG_VERSION=1.18
 PKG_NAME=gdbm-${PKG_VERSION}
 PKG_DIR_NAME=gdbm-${PKG_VERSION}
 PKG_TYPE=.tar.gz
