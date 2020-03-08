860de482b60cd11936685375b6a87b64e5bab877
Furniel <furniel@mail.ru>
2020-03-08 10:06:14 +0200

    expat: updated to 2.2.9

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.2.8
+PKG_VERSION=2.2.9
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
