6655cba2d9eefe5ed12bfacfcd3027682f660f80
Furniel <furniel@mail.ru>
2020-08-07 20:11:59 +0300

    sqlite: updated to 3320300

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=3310100
+PKG_VERSION=3320300
 PKG_NAME=sqlite-${PKG_VERSION}
 PKG_DIR_NAME=sqlite-autoconf-${PKG_VERSION}
 PKG_TYPE=.tar.gz
