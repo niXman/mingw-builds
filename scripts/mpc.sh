3e561c891ee237dc7cc11dfaccd4892e8ad41f2f
Furniel <furniel@mail.ru>
2021-05-03 11:57:20 +0300

    mpc: updated to 1.2.1

--- scripts/mpc.sh
+++ scripts/mpc.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=1.1.0
+PKG_VERSION=1.2.1
 PKG_NAME=$BUILD_ARCHITECTURE-mpc-${PKG_VERSION}-$LINK_TYPE_SUFFIX
 PKG_DIR_NAME=mpc-${PKG_VERSION}
 PKG_TYPE=.tar.gz
