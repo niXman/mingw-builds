4b15c31ae5a7f844d09c2170b2549d4a015f280d
Sizhe Zhao <prc.zhao@outlook.com>
2021-08-11 17:57:23 +0800

    GCC: use --enable-host-shared Building 'static GCC' now correctly means to build GCC with static host libraries instead of static target libraries since it runs on host, not target.

--- library/config.sh
+++ library/config.sh
@@ -87,10 +87,11 @@ PROCESSOR_OPTIMIZATION_ARCH_64='nocona'
 
 # **************************************************************************
 
-LINK_TYPE_BOTH="--enable-shared --enable-static"
 LINK_TYPE_SHARED="--enable-shared --disable-static"
 LINK_TYPE_STATIC="--enable-static --disable-shared"
-LINK_TYPE_GCC=$LINK_TYPE_BOTH
+LINK_TYPE_GCC_SHARED="--enable-host-shared"
+LINK_TYPE_GCC_STATIC="--disable-host-shared"
+LINK_TYPE_GCC=$LINK_TYPE_GCC_SHARED
 GCC_DEPS_LINK_TYPE=$LINK_TYPE_STATIC
 
 ENABLE_LANGUAGES='c,c++,fortran'
