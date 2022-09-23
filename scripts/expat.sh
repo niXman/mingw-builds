7432506c34deb60b67e2ebf731de672df763d99a
starg2 <75976488+starg2@users.noreply.github.com>
2022-09-23 19:27:18 +0900

    Update expat to 2.4.9

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.4.7
+PKG_VERSION=2.4.9
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
